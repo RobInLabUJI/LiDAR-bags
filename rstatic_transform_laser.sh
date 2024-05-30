4cdcfaeef03168dfa64fd00aad312414b100bafe
Enric Cervera <ecervera@uji.es>
2024-05-30 11:21:01 +0200

    Fix typo

--- rstatic_transform_laser.sh
+++ rstatic_transform_laser.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it --network host --volume ./bags:/bags osrf/ros:noetic-desktop-full /bags/publish_static_transform_lase.sh
+docker run --rm -it --network host --volume ./bags:/bags osrf/ros:noetic-desktop-full /bags/publish_static_transform_laser.sh
